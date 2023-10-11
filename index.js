2083eb2d95735ab2b557ace2acc6fe2830798e46
Maksimdon1 <maksimkozyrev056@gmail.com>
2023-10-11 21:53:04 +0300

    1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = 3004;
+const port = process.env.PORT || 3001;
 var md5 = require("md5");
 var sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
