b70e4fbe068838369fecc7460417d2ad9e88c026
Maksimdon1 <maksimkozyrev056@gmail.com>
2023-10-12 20:29:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -50,15 +50,11 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
 module.exports = db;
 
 app.use(
-  function (request, response, next) {
   express.urlencoded({limit: '50mb', extended: true}),
   express.urlencoded(),
-  
   cors({
-  
-
+    origin: "http://localhost:3000",
   })
-}
 );
 
 app.post("/api/register", async (req, res) => {
